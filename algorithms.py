5e5426e62ddfa6f35452da68e612adfdc072a7bb
bukevsky <bykaplay@gmail.com>
2022-11-30 22:03:03 +0300

    add merge sort

--- algorithms.py
+++ algorithms.py
@@ -9,16 +9,6 @@ def generate_list(number_of_elements):
     return list_
 
 
-def insertion_sort(list_):
-    for i in range(1,len(list_)):
-        for j in range(i,0,-1):
-            if list_[j] < list_[j-1]:
-                list_[j], list_[j-1] = list_[j-1], list_[j]
-            else:
-                break
-    return list_
-
-
 def Bubble_Sort(list_):
     for run in range(len(list_)-1):
         for i in range(len(list_)-1-run):
@@ -37,4 +27,43 @@ def selection_sort(list_):
     return list_
 
 
+def insertion_sort(list_):
+    for i in range(1,len(list_)):
+        for j in range(i,0,-1):
+            if list_[j] < list_[j-1]:
+                list_[j], list_[j-1] = list_[j-1], list_[j]
+            else:
+                break
+    return list_
 
+def merge(left_list, right_list):
+    sorted_list = []
+    left_list_index = right_list_index = 0
+    left_list_length, right_list_length = len(left_list), len(right_list)
+    for _ in range(left_list_length + right_list_length):
+        if left_list_index < left_list_length and right_list_index < right_list_length:
+            if left_list[left_list_index] <= right_list[right_list_index]:
+                sorted_list.append(left_list[left_list_index])
+                left_list_index += 1
+            else:
+                sorted_list.append(right_list[right_list_index])
+                right_list_index += 1
+        elif left_list_index == left_list_length:
+            sorted_list.append(right_list[right_list_index])
+            right_list_index += 1
+        elif right_list_index == right_list_length:
+            sorted_list.append(left_list[left_list_index])
+            left_list_index += 1
+    return sorted_list
+
+def merge_sort(nums):
+    if len(nums) <= 1:
+        return nums
+    mid = len(nums) // 2
+    left_list = merge_sort(nums[:mid])
+    right_list = merge_sort(nums[mid:])
+    return merge(left_list, right_list)
+
+
+random_list_ = generate_list(100)
+print(merge_sort(random_list_))
